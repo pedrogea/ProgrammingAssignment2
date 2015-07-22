a96b9f095a5f63c8cc2b1dc2474433a63b2707cb
pedrogea <pedrogea@users.noreply.github.com>
2015-07-22 17:07:47 +0200

    Functions makeCacheMatrix and cacheSolve are implemented.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+# Functions to increase calculations involving matrix inverses.
+# A "cache matrix" is defined, that can cache its inverse.
+
+# 'makeCacheMatrix' returns an extended matrix object
+# with differents methods to interact with its content.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    # ensures that 'x' is a matrix
+    stopifnot(inherits(x, "matrix")) 
+    # at this environment in which 'x' exists
+    # the inverse matrix (xInv) and some methods are defined
+    xInv <- NULL
+    set <- function(m) {
+        x <<- m
+        xInv <<- NULL # deletes a previous cache
+    }
+    get <- function() x
+    setInverse <- function(mInv) xInv <<- mInv
+    getInverse <- function() xInv
+    # a list with the methods is returned
+    # x and xInv are accessible via lexical scoping
+    cacheMatrix <- list(set = set, 
+         get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
+    # the output is defined as class "cacheMatrix"
+    class(cacheMatrix) <- "cacheMatrix"
+    cacheMatrix
 }
 
-
-## Write a short comment describing this function
+# 'cacheSolve' returns the inverse of a "cache matrix"
+# from its cache and builds the cache if it not exist.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # ensures that 'x' is a cacheMatrix
+    stopifnot(inherits(x, "cacheMatrix")) 
+    cInv <- x$getInverse()
+    if(!is.null(cInv)) {
+        message("using the cached inverse")
+        return(cInv)
+    }
+    # as the cache does not exist
+    # the inverse is calculated and cached
+    c <- x$get()
+    cInv <- solve(c, ...)
+    x$setInverse(cInv)
+    cInv
 }
+
+# Example:
+#   mt <- makeCacheMatrix(matrix(rnorm(4), nrow=2, ncol=2))
+#   cacheSolve(mt) # first time
+#   cacheSolve(mt) # second time (use cache)
